e52a84f065339546de0b36b5ce358fc34d9feb51
SantanuLayek <santanulayek2021@gmail.com>
2023-08-21 20:40:25 +0530

    slight experimentation. reverted back to original

--- DOM.js
+++ DOM.js
@@ -1,19 +1,21 @@
 let rock = document.getElementById('rock');
 
-
+let heading = document.getElementById('name');
 
 rock.children[1].addEventListener('click', ()=>{
     document.body.style.transition = "all 0.75s ease-in-out";
     document.body.style.backgroundColor = "black";
     document.body.style.color = "white";
+    // heading.style.animation = "boxglow1 1s linear infinite"
 })
-
+ 
 let scissor = document.getElementById('scissors');
 
 scissor.children[1].addEventListener('click', () =>{
     document.body.style.transition = "all 0.75s ease-in-out";
     document.body.style.backgroundColor = "gray";
     document.body.style.color = "black"; 
+    // heading.style.animation = "boxglow2 1s linear infinite"
 })
 
 let paper = document.getElementById('paper');
@@ -23,4 +25,5 @@ paper.children[1].addEventListener('click', () =>{
     document.body.style.transition = "all 0.75s ease-in-out";
     document.body.style.backgroundColor = "white";
     document.body.style.color = "seagreen"; 
+    // heading.style.animation = "boxglow3 1s linear infinite"
 })
